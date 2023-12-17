bda17389a12f4870e06f788046f544f028c5d522
rage73 <raghavagrawal2000ra@gmail.com>
2023-12-17 06:30:50 +0530

    01 backend cors

--- week-2/Week-2-Assignments/02-nodejs/solutions/todoServer.solution.file.js
+++ week-2/Week-2-Assignments/02-nodejs/solutions/todoServer.solution.file.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 function findIndex(arr, id) {
@@ -84,7 +86,7 @@ app.delete('/todos/:id', (req, res) => {
 
   fs.readFile("todos.json", "utf8", (err, data) => {
     if (err) throw err;
-    const todos = JSON.parse(data);
+    let todos = JSON.parse(data);
     const todoIndex = findIndex(todos, parseInt(req.params.id));
     if (todoIndex === -1) {
       res.status(404).send();
